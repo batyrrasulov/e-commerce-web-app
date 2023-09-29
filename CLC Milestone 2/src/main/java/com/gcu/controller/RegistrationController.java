52af1c79ccf0c7e9863d5013e43ed5d8046bb990
Batyr Rasulov <BRasulov@my.gcu.edu>
2023-09-29 12:11:22 -0700

    linked the login page to register, edited the register/login page to match the styles, and resolved some errors

--- CLC Milestone 2/src/main/java/com/gcu/controller/RegistrationController.java
+++ CLC Milestone 2/src/main/java/com/gcu/controller/RegistrationController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.gcu.model.UserModel;
 import com.gcu.util.DataWriter;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -22,11 +25,11 @@ public class RegistrationController {
         return "register";
     }
     @PostMapping("/doRegister")
-    public String doRegister(UserModel userModel, BindingResult bindingResult, Model model) {
+    public String doRegister(@ModelAttribute("user") @Valid UserModel userModel, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "register";
         }
         dataWriter.writeData(userModel);
-        return "register";
+        return "redirect:/register"; // Redirect to the registration form after successful registration
     }
 }
